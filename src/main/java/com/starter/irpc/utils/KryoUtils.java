14d2ef0caac686baab43543febc681f87682b7d1
julyAndSunday <53673884+lu199911@users.noreply.github.com>
2021-09-17 15:54:11 +0800

    解决kryo线程不安全

--- src/main/java/com/starter/irpc/utils/KryoUtils.java
+++ src/main/java/com/starter/irpc/utils/KryoUtils.java
@@ -14,10 +14,18 @@ import java.io.*;
  * @Date: 2021-09-17 11:27
  **/
 public class KryoUtils {
-    private static Kryo kryo = new Kryo();
+    //kryo池 本身线程不安全 需要每一个线程一个kryo
+    private static final ThreadLocal<Kryo> kryos = new ThreadLocal<Kryo>() {
+        protected Kryo initialValue() {
+            Kryo kryo = new Kryo();
+            // configure kryo instance, customize settings
+            return kryo;
+        };
+    };
 
 
     public static byte[] serialize(Object o){
+        Kryo kryo = kryos.get();
         kryo.register(o.getClass());
         Output output = new Output(1024,-1);
         kryo.writeObject(output,o);
@@ -25,6 +33,7 @@ public class KryoUtils {
     }
 
     public static  <T> T  deserialize(byte[] bytes,Class<T> clazz){
+        Kryo kryo = kryos.get();
         Input input = new Input(bytes);
         return kryo.readObject(input,clazz);
     }
